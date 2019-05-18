faf27aa9427dfedb77171dc202af138a9f1bd343
Anton Begehr <a.begehr@gmail.com>
2019-05-18 13:36:52 +0200

    add map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import styles from './Map.scss';
+import { MapView } from 'expo';
 
 function Map(props) {
   return (
-    <View style={styles.Map}>
-        <Text>
-            Hello guys.
-        </Text>
+    <View style={styles.container}>
+      <MapView
+      style={styles.Map}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
     </View>
   );
 }
