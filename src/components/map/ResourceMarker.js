13376517bc1822b5a0a2c39c98684e7c90218401
jakobkrause <jakobkrause@zedat.fu-berlin.de>
2019-05-19 01:39:28 +0200

    Formated ResourceMarker

--- src/components/map/ResourceMarker.js
+++ src/components/map/ResourceMarker.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { MapView } from 'expo';
 // import styles from './ResourceMarker.scss';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, StyleSheet} from 'react-native';
 
 const { Marker } = MapView;
 
 export default function ResourceMarker(props) {
   const { resource } = props;
-  const { title, description, place } = resource;
+  const { title, description, place, stock } = resource;
 
   const coordinate = {
     latitude: place.location.latitude,
     longitude: place.location.longitude
   };
+
+  const styles = StyleSheet.create({
+    baseText: {
+      fontFamily: 'Cochin',
+    },
+    titleText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+  });
 
   return (
     <Marker
@@ -22,14 +32,19 @@ export default function ResourceMarker(props) {
     <View style={{backgroundColor: "red", padding: 10}}>
     <Text>{title}</Text>
     </View>
-    <MapView.Callout>
-      <View>
-      <Text>
-        {description}
-      </Text>
-      <Button title='Click Me!' onPress={() => console.log('Clicked')} />
-      </View>
-      </MapView.Callout>
+    <MapView.Callout >
+    <View style={{backgroundColor: "white", padding: 3, minWidth: 200}}>
+    <Text style={styles.titleText}>
+    {title}{'\n'}
+    </Text>
+    <Text>
+    Amount: {stock.value} {stock.unit} {'\n'}
+    Details: {description}
+    </Text>
+    <Button title='Aggregate' onPress={() => console.log('Clicked')} />
+    </View>
+    </MapView.Callout>
     </Marker>
   );
+
 }
