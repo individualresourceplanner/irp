f48695dea413e9f45b6e3c74ee2cf57b5298be72
luisebenkert <luise.benkert@student.hpi.de>
2019-05-18 13:16:03 +0200

    Add sass.

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import styles from './Map.scss';
 
 function Map(props) {
   return (
-    <View >
+    <View style={styles.Map}>
         <Text>
             Hello guys.
         </Text>
