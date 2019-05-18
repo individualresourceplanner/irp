0a38b79dc7accfb472c13057aec62de9da4727bb
Anton Begehr <a.begehr@gmail.com>
2019-05-18 13:50:30 +0200

    lint

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import styles from './Map.scss';
+import { View } from 'react-native';
 import { MapView } from 'expo';
+import styles from './Map.scss';
 
-function Map(props) {
+function Map() {
   return (
     <View style={styles.container}>
       <MapView
-      style={styles.Map}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+        style={styles.Map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
--- src/state/reducer.js
+++ src/state/reducer.js
@@ -4,7 +4,6 @@ const initialState = {
 };
 
 function reducer(state = initialState, action) {
-
   switch (action.type) {
     default:
       return state;
