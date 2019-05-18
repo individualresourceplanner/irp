c07353d0271ecbf336e0a20d9670247e1be23d7d
Anton Begehr <a.begehr@gmail.com>
2019-05-18 14:19:31 +0200

    use google maps, show user location, center on Cameron, satelllite, etc.

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -3,17 +3,29 @@ import { View } from 'react-native';
 import { MapView } from 'expo';
 import styles from './Map.scss';
 
+const { PROVIDER_GOOGLE } = MapView;
+
+const initialRegion = {
+  latitude: 7.3697,
+  longitude: 12.3547,
+  latitudeDelta: 1.5,
+  longitudeDelta: 0.75,
+};
+
 function Map() {
   return (
     <View style={styles.container}>
       <MapView
         style={styles.Map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        showsMyLocationButton
+        showsPointsOfInterest
+        showsCompass
+        showsBuildings
+        showsIndoors
+        mapType="satellite"
+        initialRegion={initialRegion}
       />
     </View>
   );
